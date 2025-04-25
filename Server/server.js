486770ea90e8579a411b931bf91562bed747e700
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-04-25 21:56:28 +0530

    final commit

---
+++ Server/server.js
@@ -0,0 +1,36 @@
+import cloudinary from 'cloudinary';
+import Razorpay from 'razorpay';
+import dotenv from 'dotenv';
+dotenv.config({path:'./config/config.env'});
+import connectionToDB from './config/dbConnection.js';
+import app from './app.js';
+
+const PORT= process.env.PORT || 5000;
+
+//intialize cors
+import cors from 'cors';
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+/**
+ * @Cloudinary configuration for file storage service
+ */
+cloudinary.v2.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+    secure:process.env.CLOUDINARY_SECURE,
+})
+/**
+ * @Razorpay configuration for payment gateway
+ */
+export const razorpay =new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_SECRET,
+})
+app.listen(PORT,async ()=>{
+    await connectionToDB();
+    console.log(`App is running at  http:localhost:${PORT} `);
+})
